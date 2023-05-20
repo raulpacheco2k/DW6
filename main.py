ca5cc3cfaa85d7fb4104ec1bc7a0f123e27d4a2d
Raul Pacheco <eu@raulpacheco.com.br>
2023-05-20 00:46:15 -0300

    WIP

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import json
 from datetime import datetime, timedelta
 
 import boto3
@@ -7,28 +6,23 @@ from rich import print
 from rich.console import Console
 from rich.table import Table
 
+# Typer Configure
 app = typer.Typer()
 console = Console()
 
 # AWS Configure
 client = boto3.client('logs')
-log_group_name = '/aws/lambda/my-lambda-function'
-start_time = (datetime.now() - timedelta(hours=3)).strftime('%Y-%m-%d %H:%M:%S')
-end_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+log_group_name = '/aws/lambda/transformacao'
+end_time = datetime.now()
+start_time = end_time - timedelta(hours=3)
+query = "fields @timestamp, @message, @logStream, @log" \
+        "| sort @timestamp desc"
 
 
 @app.command()
 def homologation():
-    json_array = json.dumps(
-        [
-            {'message': 'Transtype RPJ001 associada ao lançamento'},
-            {'message': 'Transtype RPJ002 associada ao lançamento'},
-            {'message': 'Transtype RPJ002 associada ao lançamento'},
-            {'message': 'Transtype RPJ003 associada ao lançamento'}
-        ]
-    )
-
-    print(datetime.now() - timedelta(hours=3))
+    print(
+        f"Ambiente: [bold green]Homologação[/bold green]\nProcurar evento de {start_time.strftime('%Y-%m-%d %H:%M:%S')} até {end_time.strftime('%Y-%m-%d %H:%M:%S')}\n")
 
     response = client.filter_log_events(
         logGroupName=log_group_name,
@@ -36,12 +30,14 @@ def homologation():
         endTime=int(end_time.timestamp() * 1000)
     )
 
-    print(f"Ambiente: [bold green]Homologação[/bold green]\nProcurar evento de {start_time} até {end_time}")
+    table = Table(title="Homologação")
 
-    table = Table("Transctionid(s)", "6239", "6253", "Transtype(s)", "Envio para FE3", "Envio para motores")
+    table.add_column("Transtype gerada", justify="center")
 
-    for item in json.loads(json_array):
-        table.add_row("ea33d327-6fb9-4c8a-af69-3c9e79f59d49", "Ok", "Ok", item['message'], "Ok", "Ok")
+    for event in response['events']:
+        if event['message'][:3] == "END":
+            table.add_row(event['message'].replace("\n", ""))
+
     console.print(table)
 
 
